19ac83d6fa621c6ddf20637d272062f244aa8a43
jorbastos <jorbastos@yahoo.com.br>
2025-09-16 18:39:41 -0300

    Arquivo separado para Mensagens

--- webapp/ext/controller/ListReportExt.controller.js
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -1,39 +1,13 @@
 sap.ui.define([
-    "sap/m/MessageToast",
-    "sap/ui/core/message/Message",
-    "sap/ui/core/MessageType",
     "sap/m/MessageBox",
-    "sap/m/Dialog",
-    "sap/m/Button",
     "sap/m/Label",
-    "sap/m/library",
-    "sap/m/Text",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageItem",
-    "sap/m/MessageView",
-    "sap/ui/core/IconPool",
-    "sap/m/Bar",
-    "sap/m/Title",
-    "sap/ui/core/library",
     "sap/m/Input",
+    "valgroup/com/zui5coaamx/ext/custom/MessageHelper"  // caminho relativo ao webapp
 ], function (
-    MessageToast,
-    Message,
-    MessageType,
     MessageBox,
-    Dialog,
-    Button,
     Label,
-    mobileLibrary,
-    Text,
-    JSONModel,
-    MessageItem,
-    MessageView,
-    IconPool,
-    Bar,
-    Title,
-    coreLibrary,
     Input,
+    MessageHelper
 ) {
     'use strict';
 
@@ -41,6 +15,10 @@ sap.ui.define([
 
         onInit: function () {
             this.addCustomToolbarContent();
+        },
+
+        getResourceBundle: function () {
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle();
         },
 
         addCustomToolbarContent: function () {
@@ -88,7 +66,7 @@ sap.ui.define([
                 urlParameters: oKeys,
                 success: function (oData, oResponse) {
                     that.getView().setBusy(false);
-                    that.showMessages(oResponse);
+                    MessageHelper.showMessages(oResponse);
 
                 }.bind(this),
                 error: function (oError) {
@@ -100,109 +78,5 @@ sap.ui.define([
             });
         },
 
-        // LÓGICA PARA EXIBIÇÃO DAS MENSAGENS
-        getResourceBundle: function () {
-            return this.getOwnerComponent().getModel("i18n").getResourceBundle();
-        },
-
-        showMessages: function (pResponse) {
-
-            var that = this,
-                tableMessage = [],
-                i18nTexts = this.getResourceBundle(),
-                oModelMessage = new JSONModel(),
-                oMessage = JSON.parse(pResponse.headers["sap-message"]);
-
-            if (oMessage.details) {
-                tableMessage = oMessage.details;
-            }
-
-            tableMessage.push(oMessage);
-
-            for (var i = 0; i < tableMessage.length; i++) {
-                tableMessage[i].type = tableMessage[i].severity.substring(0, 1).toUpperCase() + tableMessage[i].severity.substring(1);
-            }
-
-            if (tableMessage.length == 1) {
-                var lineMessage = tableMessage[0];
-
-                switch (true) {
-                    case (lineMessage.type == "Success"):
-                        MessageToast.show(lineMessage.message);
-                        break;
-                    case (lineMessage.type == "Error"):
-                        MessageBox.error(lineMessage.message);
-                        break;
-                    default:
-                        MessageBox.warning(lineMessage.message);
-                        break;
-                }
-                return;
-            }
-
-            // Template para a Mensagem
-            var oMessageTemplate = new MessageItem({
-                type: '{type}',
-                title: '{message}',
-                description: '{description}',
-                subtitle: '{subtitle}',
-                counter: '{counter}',
-                markupDescription: '{markupDescription}',
-                groupName: '{groupName}'
-            });
-
-            oModelMessage.setData(tableMessage);
-
-            this.oMessageView = new MessageView({
-                showDetailsPageHeader: false,
-                itemSelect: function () {
-                    oBackButton.setVisible(true);
-                },
-                items: {
-                    path: "/",
-                    template: oMessageTemplate
-                },
-                groupItems: true
-            });
-
-            var oBackButton = new Button({
-                icon: IconPool.getIconURI("nav-back"),
-                visible: false,
-                press: function () {
-                    that.oMessageView.navigateBack();
-                    this.setVisible(false);
-                }
-            });
-
-            this.oMessageView.setModel(oModelMessage);
-
-            this.oDialog = new Dialog({
-                resizable: true,
-                content: this.oMessageView,
-                state: 'Error',
-                beginButton: new Button({
-                    press: function () {
-                        this.getParent().close();
-                    },
-                    text: i18nTexts.getText("close"),
-                }),
-                customHeader: new Bar({
-                    contentLeft: [oBackButton],
-                    contentMiddle: [
-                        new Title({
-                            text: i18nTexts.getText("log"),
-                            level: coreLibrary.TitleLevel.H1
-                        })
-                    ]
-                }),
-                contentHeight: "50%",
-                contentWidth: "23%",
-                verticalScrolling: false
-            });
-
-            this.oMessageView.navigateBack();
-            this.oDialog.open();
-
-        },
     };
 });
---
+++ webapp/ext/custom/MessageHelper.js
@@ -0,0 +1,151 @@
+sap.ui.define([
+  "sap/m/MessageToast",
+  "sap/m/MessageBox",
+  "sap/m/Dialog",
+  "sap/m/Button",
+  "sap/ui/model/json/JSONModel",
+  "sap/m/MessageItem",
+  "sap/m/MessageView",
+  "sap/ui/core/IconPool",
+  "sap/m/Bar",
+  "sap/m/Title",
+  "sap/ui/core/library",
+  "sap/ui/core/Component"
+],
+  function (
+    MessageToast,
+    MessageBox,
+    Dialog,
+    Button,
+    JSONModel,
+    MessageItem,
+    MessageView,
+    IconPool,
+    Bar,
+    Title,
+    coreLibrary,
+    Component
+  ) {
+    "use strict";
+
+    return {
+
+      getI18nBundle: function () {
+
+        var aComponents = Component.registry && Component.registry.all();
+        if (aComponents) {
+          var aKeys = Object.keys(aComponents);
+          for (var i = 0; i < aKeys.length; i++) {
+            var oComponent = aComponents[aKeys[i]];
+            if (oComponent.sId == 'application-app-preview-component' ||
+                oComponent.sId.includes('zui5coaamx') ) {
+              return oComponent.getModel("i18n").getResourceBundle();
+            }
+          }
+        }
+        return null;
+
+      },
+
+      showMessages: function (pResponse) {
+
+        var that = this,
+          tableMessage = [],
+          i18nTexts = this.getI18nBundle(),
+          oModelMessage = new JSONModel(),
+          oMessage = JSON.parse(pResponse.headers["sap-message"]);
+
+        if (oMessage.details) {
+          tableMessage = oMessage.details;
+        }
+
+        tableMessage.push(oMessage);
+
+        for (var i = 0; i < tableMessage.length; i++) {
+          tableMessage[i].type = tableMessage[i].severity.substring(0, 1).toUpperCase() + tableMessage[i].severity.substring(1);
+        }
+
+        if (tableMessage.length == 1) {
+          var lineMessage = tableMessage[0];
+
+          switch (true) {
+            case (lineMessage.type == "Success"):
+              MessageToast.show(lineMessage.message);
+              break;
+            case (lineMessage.type == "Error"):
+              MessageBox.error(lineMessage.message);
+              break;
+            default:
+              MessageBox.warning(lineMessage.message);
+              break;
+          }
+          return;
+        }
+
+        // Template para a Mensagem
+        var oMessageTemplate = new MessageItem({
+          type: '{type}',
+          title: '{message}',
+          description: '{description}',
+          subtitle: '{subtitle}',
+          counter: '{counter}',
+          markupDescription: '{markupDescription}',
+          groupName: '{groupName}'
+        });
+
+        oModelMessage.setData(tableMessage);
+
+        this.oMessageView = new MessageView({
+          showDetailsPageHeader: false,
+          itemSelect: function () {
+            oBackButton.setVisible(true);
+          },
+          items: {
+            path: "/",
+            template: oMessageTemplate
+          },
+          groupItems: true
+        });
+
+        var oBackButton = new Button({
+          icon: IconPool.getIconURI("nav-back"),
+          visible: false,
+          press: function () {
+            that.oMessageView.navigateBack();
+            this.setVisible(false);
+          }
+        });
+
+        this.oMessageView.setModel(oModelMessage);
+
+        this.oDialog = new Dialog({
+          resizable: true,
+          content: this.oMessageView,
+          state: 'Error',
+          beginButton: new Button({
+            press: function () {
+              this.getParent().close();
+            },
+            text: i18nTexts.getText("close"),
+          }),
+          customHeader: new Bar({
+            contentLeft: [oBackButton],
+            contentMiddle: [
+              new Title({
+                text: i18nTexts.getText("log"),
+                level: coreLibrary.TitleLevel.H1
+              })
+            ]
+          }),
+          contentHeight: "50%",
+          contentWidth: "23%",
+          verticalScrolling: false
+        });
+
+        this.oMessageView.navigateBack();
+        this.oDialog.open();
+
+      },
+
+    };
+  });
